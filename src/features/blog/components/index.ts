9eec70afac1f27d5998d80133aaf257d6bbd0026
anik <anik@anik.anik>
2026-05-20 19:16:08 +0300

    rm unused

--- src/features/blog/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { PostCard } from './PostCard'
-export { PostContent } from './PostContent'
-export { PostMeta } from './PostMeta'
-export { Sidebar } from './Sidebar'
-export { Pagination } from './Pagination'
